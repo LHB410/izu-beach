bf55d5aa2cd4f0fed09b6b957fa7d308146d6761
Laura Brooks <laura.brooks0@outlook.com>
2023-02-01 19:55:07 +0900

    push new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,18 +12,18 @@ Beach.destroy_all
 
 10.times do
   beach = Beach.create!(
-    name: "test",
-    address: "test",
+    name: Faker::Address.city + " Beach",
+    address: Faker::Address.full_address,
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto ut sunt exercitationem quia officia est reiciendis, blanditiis incidunt tempore eos maxime necessitatibus voluptatem perspiciatis beatae voluptates eligendi vitae iste sint",
     parking: [true, false].sample,
     bbq_friendly: [true, false].sample
   )
   5.times do
     beach.reviews.create!(
-      title: "test",
-      content: "test",
+      title: ["Great", "Wonderful!", "Awful", "Best beach ever!", "Worst day of my life", "Too much sand.."].sample,
+      content: Faker::Restaurant.review,
       rating: [1,2,3,4,5].sample,
-      screen_name: Faker::name
+      screen_name: Faker::Name.name
     )
   end
 end
