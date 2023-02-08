310ee1f3487aa820c51d99d600bbfbcb3e0c4130
Laura Brooks <laura.brooks0@outlook.com>
2023-02-08 23:37:21 +0900

    fixed boostrap import issue

--- izu-beach-front/src/App.js
+++ izu-beach-front/src/App.js
@@ -1,4 +1,5 @@
 import './App.scss';
+
 import { Routes, Route } from 'react-router-dom'
 import { Homepage } from './components/Homepage/Homepage';
 import { About } from './components/About/About';
--- izu-beach-front/src/components/Navbar/Navbar.js
+++ izu-beach-front/src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { Container, Nav} from 'react-bootstrap';
-import './Navbar.scss'
+
 
 export default function Navbar() {
   return (
