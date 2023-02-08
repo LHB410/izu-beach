ec38d5deae1ab44e86edff066a2497e948fe2db6
Jessica Nash <jessicanashacc@gmail.com>
2023-02-08 21:41:49 +0900

    navbar component setup and custom scss, but getting error, will touch again tomorrow

---
+++ izu-beach-front/src/components/Navbar/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import { Container, Nav} from 'react-bootstrap';
+import './Navbar.scss'
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <Container fluid>
+        <Link className="nav-link" to="/"><h2>🏖️</h2></Link>
+        {/* <Navbar.Toggle aria-controls="navbarScroll" /> */}
+        {/* <Navbar.Collapse id="navbarScroll" className="justify-content-end"> */}
+          <Nav
+            className="my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="/beaches" className="mx-4">Beaches</Nav.Link>
+            <Nav.Link href="/about" className="mx-4">About</Nav.Link>
+            <Nav.Link href="/contribute" disabled className="mx-4">Contribute</Nav.Link>
+          </Nav>
+        {/* </Navbar.Collapse> */}
+      </Container>
+    </div>
+  )
+}
--- izu-beach-front/src/index.js
+++ izu-beach-front/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
-import './index.scss';
 import App from './App';
+import './index.scss';
+import './custom.scss'
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
